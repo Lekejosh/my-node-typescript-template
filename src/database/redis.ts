a181361b9561254b10d3e458cfc99fea0e300653
lekejosh <lekejosh6wf@gmail.com>
2023-10-17 11:02:18 +0100

    fix: Closing Redis gracefully

--- src/database/redis.ts
+++ src/database/redis.ts
@@ -5,34 +5,32 @@ const isDocker = process.env.DOCKER === "true";
 
 const HOST = isDocker ? REDIS_HOST_DEV : REDIS_HOST;
 
-
-const logStruct = (func:any, error:any) => {
-  return { func, file: "cacheLib", error };
+const logStruct = (func: any, error: any) => {
+    return { func, file: "cacheLib", error };
 };
 
 const client = createClient({
-  port: 6379,
-  host: HOST,
+    port: 6379,
+    host: HOST
 } as any);
 
 client.on("connect", () => {
-  console.log(":::>  Redis Connected");
+    console.log(":::>  Redis Connected");
 });
 
 client.on("ready", () => {
-  console.log(":::>  Redis is ready");
+    console.log(":::>  Redis is ready");
 });
 
-client.on("error", (err) =>
-  console.error(logStruct("<:::  Redis is not running", err))
-);
+client.on("error", (err) => console.error(logStruct("Redis is not running", err)));
 
 client.on("end", () => {
-  console.log("<:::  Client disconnected from redis");
+    console.log("Client disconnected from redis");
 });
 
 process.on("SIGINT", () => {
-  client.quit();
+    client.quit();
+    process.exit();
 });
 
 client.connect();
